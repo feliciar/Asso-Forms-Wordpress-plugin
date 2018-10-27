5deb3745b0ae105c6a080439b8ca29876dff08ac
Felicia Rosell <felicia@rosells.se>
2018-10-27 15:35:05 +0200

    Poc create form on shortcode

--- plugin-asso-forms.php
+++ plugin-asso-forms.php
@@ -3,6 +3,17 @@
 Plugin Name: Assö Anmälan
 */
 
-add_shortcode( 'asso-form', function () {
-    return 'Hello world!';
+add_shortcode('asso-form', function () {
+    echo '<h1>Anmälan</h1>';
+
+    if ( isset ($_GET['testget']) ) {
+        echo 'Tack för din anmälan!';
+    } else {
+        ?>
+        <form action="../test-har-tagit-emot-din-anmalan" method="get">
+            <input type="text" name="testget">
+            <input type="submit">
+        </form>
+        <?
+    }
 });
