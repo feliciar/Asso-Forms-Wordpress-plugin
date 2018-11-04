79fed31ba5d603ceda4184c21fe0e223ef0aa0ac
Felicia Rosell <fero@netlight.com>
2018-11-04 17:02:27 +0100

    Add support for collecting options from db and creating form

--- database/get-form-data.php
+++ database/get-form-data.php
@@ -5,16 +5,32 @@ function getData() {
     
     global $wpdb;
     $table_name = $wpdb->prefix .'assoforms_form_field';
+    $table_field_x_options = $wpdb->prefix .'assoforms_form_fields_x_options';
+    $table_options = $wpdb->prefix .'assoforms_option';
     
     $results = $wpdb->get_results( "SELECT * FROM $table_name");
-    echo 'results: ' . $results;
     if(!empty($results)) {
         foreach($results as $row){
+            $options = [];
+            $result_options = $wpdb->get_results( "SELECT * FROM $table_options
+                                                WHERE id IN 
+                                                    (SELECT option_id FROM $table_field_x_options WHERE form_field_id = $row->id)");
+            if (count( $result_options ) ) {
+                foreach($result_options as $option){
+                    $options[] = array(
+                        'value' => $option->reference,
+                        'display_name' => $option->display_name,
+                    );
+                }
+            }
             $data[] = array( 
                 'reference' => $row->reference,
                 'field_type' => $row->field_type,
                 'title' => $row->title,
                 'placeholder' => $row->placeholder_text,
+                'required' => $row->required,
+                'required_format' => $row->required_format,
+                'options' => $options,
             );
         }
     }
