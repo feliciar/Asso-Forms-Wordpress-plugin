280d2a910120d8878c62e7530a85948f768d85ae
Felicia Rosell <fero@netlight.com>
2018-11-05 20:44:13 +0100

    Get form fields from definition in form-table

--- database/get-form-data.php
+++ database/get-form-data.php
@@ -4,11 +4,20 @@ function getData() {
     $data = array();
     
     global $wpdb;
+
+    $table_form_x_fields = $wpdb->prefix .'assoforms_forms_x_fields';
     $table_name = $wpdb->prefix .'assoforms_form_field';
     $table_field_x_options = $wpdb->prefix .'assoforms_form_fields_x_options';
     $table_options = $wpdb->prefix .'assoforms_option';
-    
-    $results = $wpdb->get_results( "SELECT * FROM $table_name");
+
+    $form_id = 1;
+
+    $results = $wpdb->get_results( 
+        "SELECT $table_name.id, reference, field_type, title, placeholder_text, `required`, required_format 
+            FROM $table_name 
+            INNER JOIN $table_form_x_fields ON $table_name.id=$table_form_x_fields.form_field_id 
+            WHERE $table_form_x_fields.form_id=$form_id
+            ORDER BY position ASC");
     if(!empty($results)) {
         foreach($results as $row){
             $options = [];
