6e04e38ccb3ba015bf06ea82ac4320e66aa2ef34
Felicia Rosell <fero@netlight.com>
2018-11-10 20:39:04 +0100

    Finish of inserting form data into database

--- plugin-asso-forms.php
+++ plugin-asso-forms.php
@@ -342,40 +342,118 @@ function getDataOld() {
     return $data;
 }
 
-add_shortcode('asso-form', function ($atts, $content, $tag) {
+function insertFormDataIntoDatabase() {
     global $wpdb;
     $table_prefix = $wpdb->prefix .'assoforms_';
     $table_form = $table_prefix . 'form';
     $table_forms_x_fields = $table_prefix . 'forms_x_fields';
     $table_form_field = $table_prefix . 'form_field';
-    $table_fields_x_options = $table_prefix . 'fields_x_options';
+    $table_fields_x_options = $table_prefix . 'form_fields_x_options';
     $table_option = $table_prefix . 'option';
 
     $form_id = 1;
 
     $data = getDataOld();
 
+    $position = 0;
+
     foreach($data as $field) {
-        $form_field = $wpdb->get_results( 
-            "SELECT id
-                FROM $table_form_field 
-                WHERE $table_form_field.reference=$field->reference");
-        if (count($form_field) === 0) {
-            $wpdb->insert(
+
+        echo "SELECT id FROM $table_form_field WHERE `reference` = " . $field['reference'];
+        $form_field_id = $wpdb->get_var( 
+            $wpdb->prepare("SELECT id FROM $table_form_field WHERE `reference` = '%s'", $field['reference'])
+        );
+        echo 'id: ' . $form_field_id;
+        if (! isset($form_field_id)) {
+            $result = $wpdb->insert(
                 $table_form_field, 
                 array(
-                    'reference' => $field['reference'], 
-                    'field_type' => $field['field_type'],
-                    'title' => $field['title'],
-                    'placeholder' => $field['placeholder_text']
-                ),
-                array('%s', '%s', '%s', '%s')
+                    'reference' => $field['reference'] ?: '', 
+                    'field_type' => $field['field_type'] ?: '',
+                    'title' => $field['title'] ?: '',
+                    'placeholder_text' => $field['placeholder'] ?: '',
+                )
+            );
+
+            echo 'Insert! Success: ' . $result . '<br>';
+
+            $form_field_id = $wpdb->get_var( 
+                $wpdb->prepare("SELECT id FROM $table_form_field WHERE `reference` = '%s'", $field['reference'])
             );
         }
+        echo 'form_field_id' . $form_field_id . '<br>';
+
+        $result = $wpdb->get_var( 
+            $wpdb->prepare("SELECT id FROM $table_forms_x_fields WHERE `form_id` = %d AND `form_field_id` = %d", $form_id, $form_field_id)
+        );
+
+        if (! isset($result)) {
+            $success = $wpdb->insert(
+                $table_forms_x_fields, 
+                array(
+                    'form_id' => $form_id, 
+                    'form_field_id' => $form_field_id,
+                    'position' => $position,
+                ),
+                array('%d', '%d', '%d')
+            );
+            echo 'Inserting form x field, success: ' . $success;
+        }
+
+        if ( isset( $field['options'] ) ) {
+            foreach ( $field['options'] as $option ) {
+                $option_id = $wpdb->get_var( 
+                    $wpdb->prepare(
+                        "SELECT id FROM $table_option WHERE `reference` = %s AND `display_name` = %s", 
+                        $option['value'], $option['display_name']
+                    )
+                );
+
+                if ( ! isset( $option_id ) ) {
+                    $success = $wpdb->insert(
+                        $table_option, 
+                        array(
+                            'reference' => $option['value'], 
+                            'display_name' =>$option['display_name']
+                        ),
+                        array('%s', '%s')
+                    );
+
+                    echo 'Insrting option! scucess: ' . $success . '<br>';
+
+                    $option_id = $wpdb->get_var( 
+                        $wpdb->prepare(
+                            "SELECT id FROM $table_option WHERE `reference` = %s AND `display_name` = %s", 
+                            $option['value'], $option['display_name']
+                        )
+                    );
+                }
+
+                echo 'option_id: ' . $option_id . '<br>';
+
+                $success = $wpdb->insert(
+                    $table_fields_x_options, 
+                    array(
+                        'option_id' => $option_id, 
+                        'form_field_id' => $form_field_id,
+                    ),
+                    array('%d', '%d')
+                );
+                var_dump( array(
+                    'option_id' => $option_id, 
+                    'form_field_id' => $form_field_id,
+                ));
+
+                echo 'Inserting fields_x_options scucess: ' . $success . '<br>';
+
+            }
+        }
+        
+        $position++;
     }
+}
 
-
-
+add_shortcode('asso-form', function ($atts, $content, $tag) {
 
     if ( ! isset($atts['form-id']) || ! isset($atts['year'])  ) {
         return;
