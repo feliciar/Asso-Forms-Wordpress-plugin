67fae83ec17510d8ab6c4bd8746b7a805ab4dce2
Felicia Rosell <felicia@rosells.se>
2018-11-22 19:52:18 +0100

    Insert data thread safely without lock

--- database/send-form-data.php
+++ database/send-form-data.php
@@ -21,64 +21,85 @@ function formDataValidation() {
     return true;
 }
 
+/**
+ * Inserts a row into a table, and returns the id of the inserted row. Is thread safe.
+ * 
+ * @return boolean The id of the inserted row, or false on failure.
+ */
+function insertIntoTableAndGetId( $table, $insertArray, $format = null ) {
+    global $wpdb;
+    $count = 0;
+    while ( $count < 10 ) {
+        $id = $wpdb->get_var( "SELECT id FROM `$table` ORDER BY id DESC" ) + 1;
+        $insertArray['id'] = $id;
+        $result = $wpdb->insert( $table, $insertArray, $format );
+
+        if ( $result ) {
+            return $id;
+        }
+        sleep(1);
+        $count++;
+    }
+    unset($insertArray['id']);
+    if ( $wpdb->insert( $table, $insertArray, $format )) {
+        return $wpdb->get_var( "SELECT id FROM `$table` ORDER BY id DESC" );
+    }
+    return false;
+}
+
 function sendDataToDatabase($form_id, $year) {
     global $wpdb;
     $table_prefix = $wpdb->prefix .'assoforms_';
 
     $table_name_signup = $table_prefix . 'signup';
-    $table_lock = $table_prefix . 'lock';
 
-    $locked = '1';
-    while ($locked === '1') {
-        $locked = $wpdb->get_var( "SELECT locked FROM $table_lock" );
-        sleep(1);
+    // Create new signup in signup table
+    $signup_id = insertIntoTableAndGetId( 
+        $table_name_signup, 
+        array( 
+            'form_id' => $form_id, 
+            'year' => $year,
+        ) 
+    );
+    if ( ! $signup_id ) {
+        echo 'Something went wrong when inserting into signup table!';
+        return;
     }
-    
-    $wpdb->query( "UPDATE $table_lock SET locked = 1" );
 
-    try {
-        // Create new signup in signup table
-        $wpdb->insert( 
-            $table_name_signup, 
-            array( 
-                'form_id' => $form_id, 
-                'year' => $year,
-                )
-            );
-        $signup_id = $wpdb->get_var( $wpdb->prepare("SELECT id FROM `$table_name_signup` WHERE `form_id` = %d AND `year` = %s ORDER BY id DESC", $form_id, $year));
+    $table_name_response = $table_prefix . 'response';
+    $table_name_form_fields = $table_prefix . 'form_field';
+    $table_name_signup_x_responses = $table_prefix . 'signup_x_responses';
 
-        $table_name_response = $table_prefix . 'response';
-        $table_name_form_fields = $table_prefix . 'form_field';
-        $table_name_signup_x_responses = $table_prefix . 'signup_x_responses';
+    $data = getFormDataFromDatabase($form_id, $year);
+    foreach($data as $field) {
+        // Create new response in response table
+        // TODO use a unique id instead of a reference
+        $field_reference = $field['reference'];
+        $field_id = $wpdb->get_var( $wpdb->prepare("SELECT id FROM `$table_name_form_fields` WHERE `reference` = %s", $field_reference));
+        $response = $_POST[$field_reference] ?: '';
 
-        $data = getFormDataFromDatabase($form_id, $year);
-        foreach($data as $field) {
-            // Create new response in response table
-            // TODO use a unique id instead of a reference
-            $field_reference = $field['reference'];
-            $field_id = $wpdb->get_var( $wpdb->prepare("SELECT id FROM `$table_name_form_fields` WHERE `reference` = %s", $field_reference));
-            $response = $_POST[$field_reference];
-            $wpdb->insert(
-                $table_name_response, 
-                array(
-                    'field_id' => $field_id, 
-                    'response' => $response,
-                ),
-                array('%d', '%s')
-            );
 
-            // Create new signup - response connection
-            $response_id = $wpdb->get_var( $wpdb->prepare("SELECT id FROM `$table_name_response` WHERE `field_id` = %d AND `response` = %s ORDER BY id DESC", $field_id, $response));
-            $wpdb->insert(
-                $table_name_signup_x_responses, 
-                array(
-                    'signup_id' => $signup_id, 
-                    'response_id' => $response_id,
-                ),
-                array('%d', '%s')
-            );
+        $response_id = insertIntoTableAndGetId(
+            $table_name_response, 
+            array(
+                'field_id' => $field_id, 
+                'response' => $response,
+            ),
+            array('%d', '%s')
+        );
+        if ( ! $response_id ) {
+            echo 'Something went wrong when inserting into response table!';
+            return;
         }
-    } finally {
-        $wpdb->query( "UPDATE $table_lock SET locked = 0" );
+
+        // Create new signup - response connection
+        $wpdb->insert(
+            $table_name_signup_x_responses, 
+            array(
+                'signup_id' => $signup_id, 
+                'response_id' => $response_id,
+            ),
+            array('%d', '%s')
+        );
     }
 }
